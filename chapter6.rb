d7c260c39ed7fd305235edca0b8b83ff5992b18c
pradeepgupta <pradeepc2c@yahoo.com>
2013-05-15 17:29:09 +0530

    Added 6th chpater excersise

---
+++ chapter6.rb
@@ -0,0 +1,20 @@
+#Angry boss
+#  puts "Boss: What you wants?"
+#  answer = gets.chomp
+#  puts 'Boss: What do you mean.."#{answer}"?|? YOU ARE FIRED..'
+
+#Table of contents
+  line_width = 50
+  var1 = "Table Of Contents"
+  var2 = "Chapter 1 : Getting Started"
+  var3 = "Chapter 2 : Numbers"
+  var4 = "Chapter 3 : Letters"
+  var5 = "page 1"
+  var6 = "page 9"
+  var7 = "page 13"
+  puts(var1.center(line_width))
+  puts ''
+  puts ''
+  puts(var2.ljust(line_width) + var5)
+  puts(var3.ljust(line_width) + var6)
+  puts(var4.ljust(line_width) + var7)
